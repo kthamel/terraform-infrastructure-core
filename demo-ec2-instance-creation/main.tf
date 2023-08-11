7eb07d81f646395d91857dd3a27abf864f3125c2
Kushan Thamel <kushan.thamel@gmail.com>
2023-08-11 14:16:14 +0530

    ec2-instance configurations were added

---
+++ demo-ec2-instance-creation/main.tf
@@ -0,0 +1,6 @@
+resource "aws_instance" "iac-ec2-instance" {
+  ami             = "ami-08a52ddb321b32a8c"
+  instance_type   = "t2.micro"
+  security_groups = ["sg-079949240623ebe3d"]
+  subnet_id = "subnet-0f3a728bed8319097"
+}
