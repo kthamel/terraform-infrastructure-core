93350f89a8d32129892e347ed95c4c532930c59b
Kushan Thamel <kushan.thamel@gmail.com>
2023-08-12 17:32:35 +0530

    simple configurations were added

---
+++ demo-s3-bucket-creation/main.tf
@@ -0,0 +1,14 @@
+resource "aws_s3_bucket" "a-new-s3-bucket" {
+  bucket = "kthamel-cloud-automation-s3-${random_string.test-string.id}"
+
+  tags = {
+    Name = "Terraform"
+  }
+}
+
+resource "random_string" "test-string" {
+  length = 5
+  special = false
+  lower = true
+  upper = false
+}
