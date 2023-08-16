f8e64018affdf3b24dbcfd71cd61f270ad220e50
Kushan Thamel <kushan.thamel@gmail.com>
2023-08-16 11:25:38 +0530

    configuration for provider and version was added

---
+++ demo-provider-versioning/main.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">1.0.0"
+}
+
+resource "random_string" "random_passwd" {
+  length = 10
+  special = false
+  lower = true
+  upper = false
+  numeric = true
+}
+
+resource "aws_s3_bucket" "random-bucket" {
+  bucket = "kthamel-provider-${random_string.random_passwd.id}"
+}
---
+++ demo-provider-versioning/output.tf
@@ -0,0 +1,4 @@
+output "random-passwd" {
+  description = "Printing the random password"
+  value = random_string.random_passwd.id
+}
