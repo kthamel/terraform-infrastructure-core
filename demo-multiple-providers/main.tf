97c504edc70c6367b4c0ac33752c3154ba46b105
Kushan Thamel <kushan.thamel@gmail.com>
2023-08-16 11:39:14 +0530

    multiple provider versions were added

---
+++ demo-multiple-providers/main.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "demo_bucket" {
+  bucket = "kthamel-multiple-provider-${random_string.rand_string.id}"
+}
+
+resource "random_string" "rand_string" {
+  length = 6
+  lower = true
+  upper = false
+  numeric = true
+  special = false
+}
