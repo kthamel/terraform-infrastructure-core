457abf00968e71c795055ce523005369a0ad3b5d
Kushan Thamel <kushan.thamel@gmail.com>
2023-08-27 23:08:30 +0530

    module isses were fixed

--- demo-terraform-module-creation/module-for-rds/main.tf
+++ demo-terraform-module-creation/module-for-rds/main.tf
@@ -1,17 +1,12 @@
 resource "aws_db_instance" "kthamel-postgres" {
-  identifier              = "kthamel-postgres"
-  db_name                 = "POSTGRES"
-  username                = "userdba"
-  instance_class          = "db.t3.micro"
-  engine                  = "postgres"
-  engine_version          = "15.3"
-  allocated_storage       = 20
-  vpc_id                  = aws_vpc.cloud-automation.vpc_id
-  subnet_ids              = aws_subnet.cloud-automation-private-subnet.subnet_ids
-  password                = random_string.dbpasswd.result
-  allowed_security_groups = aws_security_group.cloud-automation-security-group.security_griup_id
-  publicly_accessible     = true
-  skip_final_snapshot     = true
+  identifier        = "kthamel-postgres"
+  db_name           = "POSTGRES"
+  username          = "userdba"
+  instance_class    = "db.t3.micro"
+  engine            = "postgres"
+  engine_version    = "15.3"
+  allocated_storage = 20
+  password          = random_string.dbpasswd.result
 }
 
 resource "random_string" "dbpasswd" {
--- demo-terraform-module-creation/module-for-rds/modules/main.tf
+++ demo-terraform-module-creation/module-for-rds/modules/main.tf
@@ -1,22 +1,11 @@
 module "postgresql" {
-  // source                           = "squareops/rds-postgresql/aws"
-  // name                             = "postgresql"
-  // db_name                          = "proddb"
-  // vpc_id                           = "vpc-047eb8acfb73"
-  // multi_az                         = "true"
-  // subnet_ids                       = ["subnet-b39cfc", "subnet-090b8d8"]
-  environment                      = "prod"
-  // kms_key_arn                      = "arn:aws:kms:region:2222222222:key/f8c8d802-a34b"
-  // engine_version                   = "15.2"
-  instance_class                   = "db.m5.large"
-  // master_username                  = "pguser"
-  allocated_storage                = "20"
-  publicly_accessible              = false
-  skip_final_snapshot              = true
-  backup_window                    = "03:00-06:00"
-  maintenance_window               = "Mon:00:00-Mon:03:00"
-  // major_engine_version             = "15.2"
-  deletion_protection              = false
-  //allowed_security_groups          = ["sg-013cbf880"]
-  final_snapshot_identifier_prefix = "final"
+  environment = "dev"
+  publicly_accessible = true
+  skip_final_snapshot = false
+  backup_window       = "03:00-06:00"
+  maintenance_window  = "Mon:00:00-Mon:03:00"
+  deletion_protection = false
+  vpc_id              = "vpc-0f56bc3804d01f5f7"
+  security_griup_id   = "sg-043ebf4a6843ed9fa"
+  subnet_ids          = ["subnet-0e94e9704e935ac91", "subnet-0a029405a1776863a"]
 }
