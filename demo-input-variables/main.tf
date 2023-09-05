2e4a6b22df7e0b60d9324bfe238bb0a18f8be1b7
Kushan Thamel <kushan.thamel@gmail.com>
2023-09-05 13:29:14 +0530

    environment variables configuration was added

---
+++ demo-input-variables/main.tf
@@ -0,0 +1,22 @@
+locals {
+  Name    = "IaC Terraform"
+  Project = "Test"
+  User    = "kthamel"
+}
+
+resource "random_string" "random-string" {
+  length  = 5
+  special = false
+  lower   = true
+  upper   = false
+}
+
+resource "aws_s3_bucket" "new-s3-bucket-1" {
+  bucket = "kthamel-cloud-automation-s3-${random_string.random-string.id}-1"
+
+  tags = {
+    Name    = "Terraform"
+    Project = "New-01"
+    User    = var.user
+  }
+}
